9f286dad49ea076cffcf283b02539734bb0b09b8
Darryl Morris <dwsmorris@users.noreply.github.com>
2015-04-15 22:59:30 +0100

    initial tests working

--- test/test-main.js
+++ test/test-main.js
@@ -1,29 +1,30 @@
 var tests = [];
 for (var file in window.__karma__.files) {
-    if (/Spec\.js$/.test(file)) {
-        tests.push(file);
-    }
+	if (window.__karma__.files.hasOwnProperty(file)) {
+		if (/Spec\.js$/.test(file)) {
+			tests.push(file);
+		}
+	}
 }
 
 requirejs.config({
-    // Karma serves files from '/base'
-    baseUrl: '/base/src',
+	// Karma serves files from '/base'
+	baseUrl: '/base/src',
 
-    paths: {
-        'jquery': '../lib/jquery',
-        'underscore': '../lib/underscore',
-    },
+	paths: {
+		'jquery': '../lib/jquery',
+		'underscore': '../lib/underscore',
+	},
 
-    shim: {
-        'underscore': {
-            exports: '_'
-        }
-    },
+	shim: {
+		'underscore': {
+			exports: '_'
+		}
+	},
 
-    // ask Require.js to load these files (all our tests)
-    deps: tests,
+	// ask Require.js to load these files (all our tests)
+	deps: tests,
 
-    // start test run, once Require.js is done
-    callback: window.__karma__.start
+	// start test run, once Require.js is done
+	callback: window.__karma__.start
 });
-
